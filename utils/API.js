f5279396d013222034b3d3ad3a1f960914b4a77c
DonSintaro <jarriaga.important@gmail.com>
2020-04-01 20:01:04 -0500

    Trying to fix

--- utils/API.js
+++ utils/API.js
@@ -3,11 +3,11 @@ import axios from "axios";
 export default {
   // Gets all books
   getBooks: function() {
-    return axios.get("/api/saved");
+    return axios.get("/api/saved", {port:(process.env.PORT || 80)});
   },
   // Deletes the book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/books/" + id, {port:(process.env.PORT || 80)});
   },
   // Saves a book to the database
   saveBook: function(bookData) {
